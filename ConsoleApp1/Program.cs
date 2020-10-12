dc604434816a676bd6507521779c4a8fc30349ec
Your Name <adam95141@gmail.com>
2020-10-12 15:21:25 +0200

    Ajout de fonction addition et soustraction

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,14 +8,36 @@ namespace ConsoleApp1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             int A = 1;
             int B = 2;
+            Add(A, B);
+            Sous(A, B);
+        }
+
+        /// <summary>
+        /// Fonctions d'addition
+        /// </summary>
+        /// <param name="val1">A</param>
+        /// <param name="val2">B</param>
+        static void Add(int val1, int val2)
+        {
             string Addition = "addition";
+            Console.WriteLine("L'"+Addition+" de " + val1 + " et " + val2 + " vaut " + (val1 + val2));
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Fonctions de soustraction
+        /// </summary>
+        /// <param name="val1">A</param>
+        /// <param name="val2">B</param>
+        static void Sous(int val1, int val2)
+        {
             string Soustraction = "soustraction";
-            Console.WriteLine("L'"+Addition+" de "+A+" et "+B+" vaut "+ (A+B));
-            Console.WriteLine("La "+Soustraction+" de "+A+" et "+B+" vaut "+(A-B));
+            Console.WriteLine("La "+Soustraction+" de " + val1 + " et " + val2 + " vaut " + (val1 - val2));
             Console.ReadLine();
         }
     }
