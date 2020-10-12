95ffeb9c6956d449137f3d043c8ec223644e55fc
Your Name <adam95141@gmail.com>
2020-10-12 14:33:41 +0200

    Premier commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 2;
+            string Addition = "addition";
+            string Soustraction = "soustraction";
+            Console.WriteLine("L'"+Addition+" de "+A+" et "+B+" vaut "+ (A+B));
+            Console.WriteLine("La "+Soustraction+" de "+A+" et "+B+" vaut "+(A-B));
+            Console.ReadLine();
+        }
+    }
+}
